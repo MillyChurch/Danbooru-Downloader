c1cb7071bbfea13d169ce4ae76fc495b17cc9bc1
MillyChurch <inssnts4@gmail.com>
2025-02-10 22:26:21 -0300

    2.0.0 - Tentando fazer sem o chromium

--- automacao.py
+++ automacao.py
@@ -1,45 +1,21 @@
-from playwright.sync_api import sync_playwright, Locator
-from concurrent.futures import ThreadPoolExecutor
-import requests
-import time
+from httpx import Client
+from bs4 import BeautifulSoup
 
-def searchImage(pesquisa: str):
-    with sync_playwright() as pw:
-        url = f"https://danbooru.donmai.us/posts?page={5}&tags={pesquisa}"
-        browser = pw.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto(url)
-        
-        x=1
-        pagex = 6
-        imgPage = page.locator("img").nth(x)
+page = 2
+pesquisa = "beatrice_(umineko)"
 
-        while pagex < 10:
-            img = page.locator("img").nth(x)
+#criando uma conexão com o site
+url = f"https://danbooru.donmai.us/posts?page={page}&tags={pesquisa}"
+cliente = Client(base_url=url)
+resposta = cliente.get("")
 
-            while img.is_visible() and img.get_attribute("class")=="post-preview-image":
-                #pega a imagem da pagina
-                imgPage = img
-                imgPage.click() 
-                everOne = page.locator("img").nth(1)
-                src = everOne.get_attribute("src")
-                # print(f"src: {imgPage}")
-                downloadImg(src,f"{pesquisa}_({pagex}-{x})")
-                page.goto(url,timeout=5000)
-                x=x+1
-                img = page.locator("img").nth(x)
-                print(url)
+#salva o html do site
+html = resposta.text
+soup = BeautifulSoup(html, 'html.parser')
 
-            x = 1
-            pagex += 1
-            url = f"https://danbooru.donmai.us/posts?page={pagex}&tags={pesquisa}"
-            page.goto(url,timeout=5000)
-            
 
-    
-def downloadImg(imageSrc:str, nomeImagem:str):
-    resposta = requests.get(imageSrc)
-    with open(f"Downloads/{nomeImagem}.jpg","wb") as file:
-        file.write(resposta.content)
+imagens = soup.find_all("img")
+print(imagens)
 
-searchImage("beatrice_(umineko)")
+with open("open.html", "w") as filex:
+    filex.write(html)
